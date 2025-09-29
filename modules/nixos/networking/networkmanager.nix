3dfb4b29421a266dbab75024932577de47fb46a1
DesDepy <github.com.passerby609@passfwd.com>
2025-09-29 17:03:27 +0200

    added openvpn

--- modules/nixos/networking/networkmanager.nix
+++ modules/nixos/networking/networkmanager.nix
@@ -1,5 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
 
  # Easiest to use and most distros use this by default.
- networking.networkmanager.enable = true;
+  networking.networkmanager = {   
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
+  };
 }
