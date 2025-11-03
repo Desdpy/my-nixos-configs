ff6b4a826f22d82a61f8d697fea55bcf8c5d9637
Desdpy <github.com.passerby609@passfwd.com>
2025-11-04 00:53:05 +0100

    restructure

--- nixos/base/desktop-environment.nix
+++ nixos/base/desktop-environment.nix
@@ -47,7 +47,7 @@
   environment.systemPackages = [
     (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
       [General]
-      background=${../wallpapers/river-city.jpg}
+      background=${../../wallpapers/river-city.jpg}
     '')
   ];
 
