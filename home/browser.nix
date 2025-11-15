f27233f6345c06b05d091b95bae82c1269d36985
Desdpy <github.com.passerby609@passfwd.com>
2025-11-15 13:58:23 +0100

    browser options

--- home/browser.nix
+++ home/browser.nix
@@ -10,8 +10,8 @@
         settings = {
           "sidebar.verticalTabs" = true;
           "sidebar.main.tools" = "history";
-          "sidebar.visibility" = "expand-on-hover";
           "browser.toolbars.bookmarks.visibility" = "never";
+          # "sidebar.visibility" = "expand-on-hover";
           # "browser.startup.page" = 3;
         };
       };
--- nixos/virtual-reality.nix
+++ nixos/virtual-reality.nix
@@ -12,54 +12,14 @@
     # Run WiVRn as a systemd service on startup
     # autoStart = true;
 
-    package = pkgs.wivrn.overrideAttrs (old: rec {  
-      version = "bdac7cafae07e8b7d9bd3a001bd81b81d63296e3";
-      src = pkgs.fetchFromGitHub {
-        owner = "notpeelz";
-        repo = "WiVRn";
-        rev = version;
-        # This will throw an error when evaluating and give you the correct hash - put that here
-        hash = "sha256-Xc2vPgYSSdtCfocNztQFyEkCH78FcFvxDeFc4KSWSLA=";
-      };
-      cmakeFlags = old.cmakeFlags ++ [
-        (lib.cmakeBool "WIVRN_FEATURE_SOLARXR" true)
-      ];
-    });
+    # If you're running this with an nVidia GPU and want to use GPU Encoding (and don't otherwise have CUDA enabled system wide), you need to override the cudaSupport variable.
+    package = (pkgs.wivrn.override { cudaSupport = true; });
 
-    # Config for WiVRn (https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md)
-    config = {
-      enable = true;
-      json = {
-        # 1.0x foveation scaling
-        scale = 1.0;
-        # 100 Mb/s
-        bitrate = 100000000;
-        encoders = [
-          {
-          encoder = "vaapi";
-          codec = "h265";
-          # 1.0 x 1.0 scaling
-          width = 1.0;
-          height = 1.0;
-          offset_x = 0.0;
-          offset_y = 0.0;
-          }
-        ]; 
-      };
-    };
-  }; 
-
-  environment.sessionVariables = lib.mkAfter {
-    WEBKIT_DISABLE_DMABUF_RENDERER = "1";
+    # You should use the default configuration (which is no configuration), as that works the best out of the box.
+    # However, if you need to configure something see https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md for configuration options and https://mynixos.com/nixpkgs/option/services.wivrn.config.json for an example configuration.
   };
-
-  services.udev.extraRules = ''
-      SUBSYSTEM=="usb", ATTR{idVendor}=="1209", ATTR{idProduct}=="7690", MODE="0666"
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="7690", MODE="0666"
-  '';
 
   environment.systemPackages = [
     pkgs.wlx-overlay-s
-    pkgs.slimevr
   ];
 }
